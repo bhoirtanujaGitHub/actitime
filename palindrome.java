9607840646b97665bcfef8e7bba4d678b50454ca
bhoirtanujaGitHub <125802987+bhoirtanujaGitHub@users.noreply.github.com>
2023-02-27 09:11:44 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,30 @@
+
+// write a java program to display palindrome number in range
+
+
+#include<stdio.h>
+//strong number convert into range using nested for loop
+main()
+{
+	int n,bup,i,fact,sum,x,j;
+	printf("\n enter the Range=");
+	scanf("%d",&x);
+	
+	for(j=1;j<=x;j++)
+	{
+	n=j; 
+	i=1;
+	sum=0;
+	
+	for(;n>0;n++)
+	{
+		
+	}
+	if(j==sum)
+	{
+		printf("\n %d is palindrome number",j);
+	}
+
+	
+}
+}
